0107de6e09eb9d9e259f3fc7f8c9a07a6c70728d
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-12-04 19:43:54 +0330

    update api

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from 'cors'
 const app = express()
 app.use(cors())
-app.get(('/api'),(req,res)=> {
+app.get(('/'),(req,res)=> {
     res.send({ msg: 'CONNECT SERVER TO CLIENT' });
 })
 app.listen(5000, () => {
