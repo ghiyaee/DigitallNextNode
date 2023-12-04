b5ba31230729b1a6631cfb4d5445eaa0c17ce721
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-12-04 19:34:05 +0330

    add vercel.json

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,19 +1,18 @@
-'use client'
-import Image from 'next/image'
-import { useEffect ,useState} from 'react'
-
+'use client';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 export default function Home() {
- const [res,setRes]=useState('LOADING...')
+  const [res, setRes] = useState('loading...');
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:5000/api/')
         .then((res) => res.json())
         .then((res) => setRes(res.msg));
-   },1000)
-  },[])
+    }, 1000);
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>{res }</div>
+      <div>{res}</div>
     </main>
-  )
+  );
 }
