c283f5cf97c84081a91ce05d5e8a05f0dfd3b2d2
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-12-30 20:47:23 +0330

    update search product

--- client/src/components/layout/SearchBr.jsx
+++ client/src/components/layout/SearchBr.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 function SearchBr() {
+  const router = useRouter();
+  const [searchProduct, setSearchProduct] = useState('');
   return (
-    <section className=''>
-      <form action="" className="">
+    <section className="">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className=" flex items-center "
+      >
         <input
           type="text"
-          className="w-full h-10 border rounded-full outline-none text-center"
-          placeholder="اینجا برند مورد نطر خود را جستجوکنید"
+          className="w-[100%] h-10 border rounded-full outline-none text-center "
+          placeholder=" برند مورد نظر"
+          onChange={(e) => setSearchProduct(e.target.value.toLocaleLowerCase())}
+          value={searchProduct}
         />
+        {searchProduct ? (
+          <Link
+            href={{
+              pathname: `/product`,
+              query: { value: searchProduct },
+            }}
+            className="relative "
+          >
+            <button
+              className="bg-primary text-white h-10 px-2
+           rounded-full absolute left-0  -translate-y-[50%] top-[50%]"
+            >
+              جستجوکن
+            </button>
+          </Link>
+        ) : (
+          ''
+        )}
       </form>
     </section>
   );
